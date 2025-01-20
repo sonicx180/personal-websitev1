8237954822b851449fe466354c53de4486779cc3
Sonicx180 <chetanzeogu410@gmail.com>
2025-01-19 19:15:03 -0800

    e

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,15 @@
 
 </script>
 
-Sonicx180
-
-Building for the Web
 
 <div class="desc">
     <h1 class = "title"> Hey there! 👋 I'm Sonicx180</h1> 
     <h2 class = "subtitle"> a software/web developer that likes to do a bit of everything</h2>
 
-
-   <p> I've been a developer for 4+ years now, and I primairly use HTML, CSS, Javascript</p>
+    <p> I've been a developer for 4+ years now, and I primairly use HTML, CSS, Javascript</p>
 
     <h2> Wanna get in touch?</h2>
-
+    <p> Head over to my contact page!</p>
 </div>
 
 <style>
