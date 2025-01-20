cbda1af68a0bc58856309be5dfa21d8acfc9c509
Sonicx180 <chetanzeogu410@gmail.com>
2025-01-19 18:56:31 -0800

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	// CSS
+    import './app.css'
+</script>
+
+<nav class = "navbar">
+    <a href = "/"> Home </a>
+    <a href = "/about"> About </a>
+    <a href = "/work"> Work</a>
+    <a href = "/contact"> Contact </a>
+</nav>
+
+<slot/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+Sonicx180
+
+Building for the Web
+
+<div class="desc">
+    <h1 class = "title"> Hey there! 👋 I'm Sonicx180</h1> 
+    <h2 class = "subtitle"> a software/web developer that likes to do a bit of everything</h2>
+
+
+   <p> I've been a developer for 4+ years now, and I primairly use HTML, CSS, Javascript</p>
+
+    <h2> Wanna get in touch?</h2>
+
+</div>
+
+<style>
+
+</style>
